a21726af68806779afe7db5edb8e3f9a907be1e3
ubuntu <ubuntu@localhost.localdomain>
2019-10-06 02:27:57 +0800

    Updates

---
+++ precache-manifest.4d8d988b5d0673efd21ba5cae98bf1f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb70078f35fa59344131bc3d5db87b1e",
+    "url": "/poker_dungeons/index.html"
+  },
+  {
+    "revision": "a0fde1dba83c0b462d46",
+    "url": "/poker_dungeons/static/css/main.acc50d3a.chunk.css"
+  },
+  {
+    "revision": "8ef85aad860a544ce422",
+    "url": "/poker_dungeons/static/js/2.d772b62b.chunk.js"
+  },
+  {
+    "revision": "a0fde1dba83c0b462d46",
+    "url": "/poker_dungeons/static/js/main.6250ab4d.chunk.js"
+  },
+  {
+    "revision": "33ede77236d5ce968f08",
+    "url": "/poker_dungeons/static/js/runtime-main.a105c94d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/poker_dungeons/static/media/logo.25bf045c.svg"
+  }
+]);
